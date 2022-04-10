49bd0edebf4c9f81ef6d07d4c1dc48cf8ac2b8b6
KazunariSekikawa <kgkz.sk@gmail.com>
2022-04-10 10:32:01 +0900

    add posts list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { apiClient } from '../src/lib/api-client'
 import styles from '../src/styles/Home.module.css'
+import { Blog } from '../src/types/apiResponse'
 
-const Home: NextPage = () => {
+type Props = {
+  contents: Blog[]
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await apiClient.blog.$get()
+  return {
+    props: { ...data },
+  }
+}
+
+const Home: NextPage<Props> = props => {
+  const { contents } = props
   return (
-    <Link href="posts">
-      <a>Post</a>
-    </Link>
+    <div className={styles.container}>
+      {contents.map(content => (
+        <div key={content.id} className={styles.content}>
+          <Link href={`/posts/${content.id}`}>
+            <a>{content.title}</a>
+          </Link>
+        </div>
+      ))}
+    </div>
   )
 }
 
