e3234d1c450060d13b40d97a1a73b6ddd653b5c8
KazunariSekikawa <kgkz.sk@gmail.com>
2022-06-14 00:48:46 +0900

    refactor: 💡 change to mui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,17 @@
+import * as React from 'react'
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import createEmotionCache from '../src/lib/createEmotionCache'
+import createEmotionServer from '@emotion/server/create-instance'
+import theme from '../src/styles/theme'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return initialProps
-  }
-
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head lang="ja">
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+      <Html lang="ja">
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          {(this.props as any).emotionStyleTags}
         </Head>
         <body>
           <Main />
@@ -25,4 +22,33 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+MyDocument.getInitialProps = async ctx => {
+  const originalRenderPage = ctx.renderPage
+
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />
+        },
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  }
+}
