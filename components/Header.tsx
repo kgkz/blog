1d6baaaf6d0d14adf56d66f1e937647f0a0ca4fd
KazunariSekikawa <kgkz.sk@gmail.com>
2022-06-30 15:51:10 +0900

    refactor: 💡 add Header and Footer

---
+++ components/Header.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import Toolbar from '@mui/material/Toolbar'
+import { IconButton, Typography } from '@mui/material'
+import { Search } from '@mui/icons-material'
+import Box from '@mui/material/Box'
+// import Link from 'next/link'
+import Link from '@mui/material/Link'
+
+interface HeaderProps {
+  title: string
+  sections: ReadonlyArray<{
+    title: string
+    path: string
+  }>
+}
+
+export default function Header(props: HeaderProps) {
+  const { sections, title } = props
+
+  return (
+    <header>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <Search />
+        </IconButton>
+      </Toolbar>
+      <Toolbar component="nav" variant="dense" sx={{ justifyContent: 'left', overflowX: 'auto' }}>
+        {sections.map(section => (
+          <Link
+            href={`/header/${section.path}`}
+            key={section.title}
+            color="inherit"
+            noWrap
+            variant="body2"
+            sx={{ px: 5, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </header>
+  )
+}
---
+++ pages/header/contact.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Typography } from '@mui/material'
+import Container from '@mui/material/Container'
+
+export default function about() {
+  return (
+    <>
+      <Container>
+        <Typography>Contact page</Typography>
+      </Container>
+    </>
+  )
+}
