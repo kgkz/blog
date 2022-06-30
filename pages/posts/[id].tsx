0ab4afe5b0eecb95a1be655bc71de8d9f323f454
Kz <kgkz.sk@gmail.com>
2022-06-30 18:52:27 +0900

    refactor: 💡 update [id] page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,16 +1,53 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import React from 'react'
+import { ReactElement } from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import ReactMarkdown from 'markdown-to-jsx'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import Box from '@mui/material/Box'
+
 import { apiClient } from '../../src/lib/api-client'
 import { Blog } from '../../src/types/apiResponse'
 
-type Props = {
-  blog?: Blog[]
+type ArticleDetailProps = {
+  blog?: Blog
   errors?: string
 }
 
-const BlogId = ({ blog }: Props) => {
-  if (!blog) return
-  return <div>{blog[0].title}</div>
+const options = {
+  overrides: {
+    h1: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'h4',
+        component: 'h1',
+      },
+    },
+    h2: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'h6', component: 'h2' },
+    },
+    h3: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'subtitle1' },
+    },
+    h4: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'caption',
+        paragraph: true,
+      },
+    },
+    p: {
+      component: Typography,
+      props: { paragraph: true },
+    },
+    a: { component: Link },
+    li: {
+      component: MarkdownListItem,
+    },
+  },
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -27,7 +64,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
     const data = await apiClient.blog.$get({ query: { ids: params?.id } })
     return {
-      props: { blog: { ...data.contents } },
+      props: { blog: data.contents.shift() },
     }
   } catch (err) {
     if (err instanceof Error) {
@@ -37,4 +74,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 }
 
-export default BlogId
+function MarkdownListItem(props: any) {
+  return <Box component="li" sx={{ mt: 1, typography: 'body1' }} {...props} />
+}
+
+export default function ArticleDetail({ blog }: ArticleDetailProps) {
+  if (!blog) return
+  return <ReactMarkdown options={options}>{blog.body}</ReactMarkdown>
+}
