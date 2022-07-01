7d72bdf3f50ed720f86b0d41c763be5e0781b66c
Kz <kgkz.sk@gmail.com>
2022-07-01 12:51:24 +0900

    feat: 🎸 add syntax-highlighter

---
+++ components/Markdown.tsx
@@ -0,0 +1,33 @@
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+type MarkdownProps = {
+  readonly markdown: string
+}
+
+export default function Markdown(props: MarkdownProps) {
+  const { markdown } = props
+  return (
+    <>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter style={atomDark} language={match[1]} PreTag="div" {...props}>
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </>
+  )
+}
